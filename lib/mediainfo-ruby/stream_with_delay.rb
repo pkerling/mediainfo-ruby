73c37a2fbb8d38d2bb822994a00f595e82162624
Philipp Kerling <pkerling@casix.org>
2015-12-24 19:27:53 +0900

    Fix copy-paste error

--- lib/mediainfo-ruby/stream_with_delay.rb
+++ lib/mediainfo-ruby/stream_with_delay.rb
@@ -1,11 +1,11 @@
 module MediaInfoLib
   module StreamWithDelay
     def delay_ms
-      @stream_info["Delay"].to_f if @stream_info["Duration"]
+      @stream_info["Delay"].to_f if @stream_info["Delay"]
     end
 
     def delay_s
-      @stream_info["Delay"].to_f / 1000.0 if @stream_info["Duration"]
+      @stream_info["Delay"].to_f / 1000.0 if @stream_info["Delay"]
     end
   end
 end
